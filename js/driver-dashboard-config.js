64a16108fb94637bd8682d9a9438381253f20f1f
Josh <joshmarcusbautista12@gmail.com>
2025-12-10 21:24:26 +0800

    UPDATE: depart button icon

--- js/driver-dashboard-config.js
+++ js/driver-dashboard-config.js
@@ -842,7 +842,7 @@ function renderDestinations() {
                 ${dest.status === 'upcoming' ? `
                     <button class="btn-depart" id="depart-btn-${dest.id}" onclick="handleDepartRide('${dest.id}')" ${!isRideTimeNow(dest.date, dest.time) ? 'disabled' : ''}>
                         <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L10.293 7.5H4.5z"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M3 1L1.66667 5H0V8H1V15H3V13H13V15H15V8H16V5H14.3333L13 1H3ZM4 9C3.44772 9 3 9.44772 3 10C3 10.5523 3.44772 11 4 11C4.55228 11 5 10.5523 5 10C5 9.44772 4.55228 9 4 9ZM11.5585 3H4.44152L3.10819 7H12.8918L11.5585 3ZM12 9C11.4477 9 11 9.44772 11 10C11 10.5523 11.4477 11 12 11C12.5523 11 13 10.5523 13 10C13 9.44772 12.5523 9 12 9Z"/>
                         </svg>
                         Depart
                     </button>
